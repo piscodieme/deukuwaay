9122fa1ed609521ce2ef32925c0c36640c9353c9
Baba Saidou DIEME <babadieme94@gmail.com>
2025-02-12 05:10:59 +0000

    retour teste 1

--- src/app/FrontOffice/Components/nos-services-page/nos-services-page.component.ts
+++ src/app/FrontOffice/Components/nos-services-page/nos-services-page.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nos-services-page',
   templateUrl: './nos-services-page.component.html',
   styleUrls: ['./nos-services-page.component.css']
 })
-export class NosServicesPageComponent {
+export class NosServicesPageComponent implements OnInit {
+
+
+  imageBreadcrumb = "./../../../assets/imgsites/immoimg1.jpg"
+  titleBreadcrumb = "Bienvenue chez vous !"
+  description = " Dëkuwaay, c'est bien plus qu'une simple agence immobilière. C’est le portail de chez vous !  Nous sommes votre allié de confiance dans la recherche du logement idéal au Sénégal. Notre engagement c’est de vous aider à trouver le lieu qui vous convient le mieux, un endroit où vous pourrez créer des souvenirs inoubliables !"
+
+  abonnements = [
+    {id:1, typeAbonnement:"Pack Premium", price: 10000},
+    {id:2, typeAbonnement:"Pack Silver", price: 15000},
+    {id:3, typeAbonnement:"Pack Gold", price: 25000},
+    {id:4, typeAbonnement:"Pack Diamond", price: 30000}
+  ]
+  
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
 }
--- src/app/Utilities/slider/slider.component.ts
+++ src/app/Utilities/slider/slider.component.ts
@@ -1,14 +1,57 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
+interface ImagesSlide{
+  src:string
+  alt:string
+}
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
   styleUrls: ['./slider.component.css']
 })
-export class SliderComponent {
+export class SliderComponent  implements OnInit{
 
   @Input({required:true}) title = ""
-  @Input({required:true}) images = ""
+  @Input() images : ImagesSlide [] = [
+    {
+      src:"./../../../assets/imgsites/immoimg1.jpg",
+      alt:"Maison 1"
+    },
+    {
+      src:"./../../../assets/imgsites/immoimg2.jpg",
+      alt:"Maison 2"
+    },
+    {
+      src:"./../../../assets/imgsites/room.jpg",
+      alt:"Chambre "
+    }
+  ]
   @Input({required:true}) description = ""
+  @Input() indicateur = true
+  @Input() autoSlide = false;
+  @Input() slideDuration = 5000;
+  selectedIndex = 0
+
+  ngOnInit(): void {
+    if(this.autoSlide){
+      this.autoSlideImage()
+    }
+  }
+  autoSlideImage() {
+    setInterval(()=>{
+      this.onNextClick();
+    }, this.slideDuration)
+  }
+  onNextClick() {
+    if(this.selectedIndex=== this.images.length-1){
+      this.selectedIndex = 0
+    }else{
+      this.selectedIndex++
+    }
+  }
+
+  selectImage(index : number) : void{
+    this.selectedIndex = index
+  }
 
 }
